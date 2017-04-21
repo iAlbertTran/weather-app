4fcd5597360bc07bfad9f6817d93d5932a76b906
AlbertTran <albtran@ucdavis.edu>
2017-04-21 02:13:08 -0700

    redid the way scrolling is achieved. Has a smooth scrolling animation now

--- weather.js
+++ weather.js
@@ -1,12 +1,115 @@
+// Global variables to be shared between the first three functions, to avoid repetitive declarations
+// Math.round() has been used on each of these variables to maintain consistency when changing the left style property
+// of each div apart of class day.
+var weatherBox;
+var weatherBoxWidth;
+var dayBox;
+var dayBoxWidth;
+var totalDayBoxWidth;
+var leftOverSpace;
+
+// startPos designates which day is the left most displayed box
+// endPos designates which day is the right most displayed box
+var startPos = 1;
+var endPos = 5;
+
+// Executed on load to get the positioning of the weather boxes equal
+function init() {
+    // gets the weatherBox div, and finds its width
+    weatherBox = document.getElementById("weatherBox");
+    weatherBoxWidth = weatherBox.clientWidth;
+
+    // gets the width of a single div for a single day. Used day 1 since all of the divs should be equal width inside
+    // the weather box
+    dayBox = document.getElementById("day1");
+    dayBoxWidth = Math.round(dayBox.offsetWidth);
+
+    // gets the total width of 5 day divs and determines the left over space in the weather box to be distributed
+    // evenly between each displayed div. There are a total of 4 gaps between the divs.
+    totalDayBoxWidth = Math.round(dayBox.offsetWidth * 5);
+    leftOverSpace = Math.round(weatherBoxWidth - totalDayBoxWidth);
+    leftOverSpace = Math.round(leftOverSpace/6);
+
+    var pos = leftOverSpace;
+
+    // for loop to set the left property of each of the first 5 days.
+    // Starts at left: 0 and then increments by leftOverSpace
+    for (var i = 0; i < 10; i++) {
+        var dayByNum = document.getElementById("day" + (i+1));
+        dayByNum.style.left = pos + "px";
+        pos = Math.round(pos + leftOverSpace);
+    }
+}
+
+document.getElementById("rightArrow").onclick = function(){
+
+    //increments to keep track of which days are being displayed
+    ++startPos;
+    ++endPos;
+
+    // the left arrow will appear if the first day is not being displayed
+    if(startPos !== 1){
+        var leftButton = document.getElementById("leftArrow").classList;
+        leftButton.remove("hide");
+    }
+
+    // the right arrow will disappear if the tenth day is being displayed
+    if(endPos === 10){
+        var rightButton = document.getElementById("rightArrow").classList;
+        rightButton.add("hide");
+    }
+
+
+    // for loop to simulate smooth scrolling right. each day will take the position of the day prior, where the left most
+    // day will be hidden and the first hidden day on the right will now be displayed
+    var pos = leftOverSpace + dayBoxWidth;
+    for(var i = 0; i<10; ++i){
+        var dayPos = parseInt(document.getElementById("day" + (i+1)).style.left);
+        dayPos = Math.round(dayPos - pos);
+        document.getElementById("day" + (i+1)).style.left = dayPos + "px";
+    }
+};
+
+document.getElementById("leftArrow").onclick = function(){
+
+    // decrements to keep track of which days are being displayed
+    --startPos;
+    --endPos;
+
+    // left button will be hidden if the first day is being displayed
+    if(startPos === 1){
+        var leftButton = document.getElementById("leftArrow").classList;
+        leftButton.add("hide");
+    }
+
+    // right button will be shown if the tenth day is not being displayed
+    if(endPos !== 10){
+        var rightButton = document.getElementById("rightArrow").classList;
+        rightButton.remove("hide");
+    }
+
+    // for loop to simulate smooth scrolling left. each day will take the position of the next day. The first hidden day
+    // on the left will be displayed and the last displayed day on the right will be hidden.
+    var pos = leftOverSpace + dayBoxWidth;
+    for(var i = 0; i<10; ++i){
+        var dayPos = parseInt(document.getElementById("day" + (i+1)).style.left);
+        dayPos = Math.round(dayPos + pos);
+        document.getElementById("day" + (i+1)).style.left = dayPos + "px";
+    }
+};
+
 
 
 /* called when new weather arrives */
-
 function callbackFunction(data) {
-    //Extracts the forecast information needed from the data
+
+    // Month array used to get the full name from an abbreviation
+    var allMonths = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+    // Extracts the forecast information needed from data
     var forecast = data.query.results.channel.item.forecast;
 
-    //Finds what the location the weather is for, then places it in the header
+    // Finds what the location the weather is for, then places it in the header
     var location = data.query.results.channel.location;
     var curr = document.getElementById("currentLocation");
     curr.textContent = JSON.parse(JSON.stringify(location.city + ", " + location.region));
@@ -15,68 +118,29 @@ function callbackFunction(data) {
     //loops through to fill in the HTML based on the day
     for (var i = 1; i < 11; ++i) {
 
-        //Designates which "day" div we're dealing with in the HTML
+        // Designates which "day" div we're dealing with in the HTML
         var day = document.getElementById("date" + i);
         var weather = document.getElementById("weather" + i);
         var temperature = document.getElementById("temp" + i);
 
-        //Takes the date and removes the year, then reverses the format from day month to month day
+        // splits the date into an array by white space
         var dateArr = forecast[i - 1].date.split(" ");
-        dateArr = dateArr.slice(0, 2);
-        var date = dateArr.pop();
-        date = date + " " + dateArr.shift();
 
-        //JSON.parse to remove quotes around the JSON String.
-        day.textContent = JSON.parse(JSON.stringify(date));
-        weather.textContent = JSON.parse(JSON.stringify(forecast[i - 1].text));
-        temperature.textContent = JSON.parse(JSON.stringify(forecast[i - 1].high));
+        // based on Yahoo's documentation, the day will be the first element
+        // and the month will be the second element in the dateArr
+        var dayOfMonth = dateArr[0];
+        var month = dateArr[1];
+
+        // A for loop to match the abbreviated month to the full name of the month in order to replace the abbreviation
+        // with the full name.
+        for(var j = 0; j < 12; ++j){
+            if(allMonths[j].indexOf(month) >= 0)
+                month = allMonths[j];
+        }
+
+
+        day.textContent = month + " " + dayOfMonth;
+        weather.textContent = forecast[i - 1].text;
+        temperature.textContent = forecast[i - 1].high;
     }
 }
-
-//counters to keep track of what days are being shown.
-var pos = 1;
-var backpos = 5;
-
-//Function for when the right arrow is clicked
-document.getElementById("rightArrow").onclick = function(){
-    //increment pos and backpos to maintain what days are being shown
-    pos += 1;
-    backpos += 1;
-
-    //if the pos is not 1 (the first day of the 10-day forecast), show the left arrow
-    if(pos !== 1){
-        var leftButton = document.getElementById("leftArrow").classList;
-        leftButton.remove("hide");
-    }
-    //if the backpos is 10 (the last day of the 10-day forecast), hide the right arrow
-    if(backpos === 10){
-        var rightButton = document.getElementById("rightArrow").classList;
-        rightButton.add("hide");
-    }
-
-    var hideDay = document.getElementById("day" + (pos-1)).classList;
-    hideDay.add("hide");
-
-    var showDay = document.getElementById("day" + backpos).classList;
-    showDay.remove("hide");
-};
-
-document.getElementById("leftArrow").onclick = function(){
-    pos -= 1;
-    backpos -= 1;
-    if(backpos !== 11){
-        var rightButton = document.getElementById("rightArrow").classList;
-        rightButton.remove("hide");
-    }
-
-    if(pos === 1){
-        var leftButton = document.getElementById("leftArrow").classList;
-        leftButton.add("hide");
-    }
-
-    var hideDay = document.getElementById("day" + (backpos+1)).classList;
-    hideDay.add("hide");
-
-    var showDay = document.getElementById("day" + pos).classList;
-    showDay.remove("hide");
-};
